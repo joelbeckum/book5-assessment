4ae4a777df03bc7ac8705245a1e9d58c49c0a2f3
Joel Beckum <joelbeckum@gmail.com>
2021-06-16 10:54:00 -0400

    Added topic element to tthe DOM populated from external API; reformatted fetch call to push all data at once to the application state

--- src/scripts/LetterApp.js
+++ src/scripts/LetterApp.js
@@ -1,4 +1,5 @@
 import { Authors } from "./Authors.js"
+import { Topics } from "./Topics.js"
 
 export const LetterApp = () => {
     return `
@@ -16,7 +17,7 @@ export const LetterApp = () => {
 
     <section class="topics">
     <h3>Topic</h3>
-
+        ${Topics()}
     </section>
 
     <section class="recipients">
--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -8,44 +8,63 @@ const applicationState = {
     topics: []
 }
 
-export const fetchAuthors = () => {
-    return fetch(`${API}/authors`)
-        .then(response => response.json())
-        .then(
-            (externalAuthors) => {
-                applicationState.authors = externalAuthors
-            }
-        )
+
+export const fetchExternalData = () => {
+    return Promise.all([
+        fetch(`${API}/authors`),
+        fetch(`${API}/letters`),
+        fetch(`${API}/topics`)
+    ]).then(responses => {
+        return Promise.all(responses.map(response => {
+            return response.json()
+        }))
+    }).then(externalData => {
+        applicationState.authors = externalData[0]
+        applicationState.letters = externalData[1]
+        applicationState.topics = externalData[2]
+    })
 }
 
 export const getAuthors = () => {
     return [...applicationState.authors]
 }
 
-export const fetchTopics = () => {
-    return fetch(`${API}/topics`)
-        .then(response = response.json())
-        .then(
-            (externalTopics) => {
-                applicationState.topics = externalTopics
-            }
-        )
-}
-
 export const getTopics = () => {
     return [...applicationState.topics]
-}
-
-export const fetchLetters = () => {
-    return fetch(`${API}/letters`)
-        .then(response = response.json())
-        .then(
-            (externalLetters) => {
-                applicationState.letters = externalLetters
-            }
-        )
 }
 
 export const getLetters = () => {
     return [...applicationState.letters]
 }
+
+// export const fetchAuthors = () => {
+//     return fetch(`${API}/authors`)
+//         .then(response => response.json())
+//         .then(
+//             (externalAuthors) => {
+//                 applicationState.authors = externalAuthors
+//             }
+//         )
+// }
+
+
+// export const fetchTopics = () => {
+//     return fetch(`${API}/topics`)
+//         .then(response = response.json())
+//         .then(
+//             (externalTopics) => {
+//                 applicationState.topics = externalTopics
+//             }
+//         )
+// }
+
+
+// export const fetchLetters = () => {
+//     return fetch(`${API}/letters`)
+//         .then(response = response.json())
+//         .then(
+//             (externalLetters) => {
+//                 applicationState.letters = externalLetters
+//             }
+//         )
+// }
